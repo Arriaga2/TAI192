70160ecb10f945308b9c61246b370aa24d25bb8f
Arriaga2 <163058678+Arriaga2@users.noreply.github.com>
2025-02-11 19:26:17 -0600

    1

--- FAST/main.py
+++ FAST/main.py
@@ -19,7 +19,6 @@ usuarios=[
 def home():
     return {'hello': 'world FastAPI'}
 
-
 #endpoint parametro obligatorio
 @app.get('/usuario/{id}', tags=['Parametro obligatorio'])
 def consultaUsuario(id:int):
@@ -29,7 +28,7 @@ def consultaUsuario(id:int):
 
 # Endpoint con parámetro opcional
 @app.get('/usuario/', tags=['Parametro Opcional'])
-def consulta_usuario_opcional(usuario_id: Optional[int] = None):
+def consultaUsuario2(usuario_id: Optional[int] = None):
     if usuario_id is not None:
         for usuario in usuarios:
             if usuario["id"] == usuario_id:
@@ -37,3 +36,25 @@ def consulta_usuario_opcional(usuario_id: Optional[int] = None):
         return {"Mensaje": f"No se encontró el usuario con id: {usuario_id}"}
     else:
         return {'Mensaje': "No se proporcionó un id"}
+    
+#endpoint con varios parametro opcionales
+@app.get("/usuarios/", tags=["3 parámetros opcionales"])
+async def consulta_usuarios(
+    usuario_id: Optional[int] = None,
+    nombre: Optional[str] = None,
+    edad: Optional[int] = None
+):
+    resultados = []
+
+    for usuario in usuarios:
+        if (
+            (usuario_id is None or usuario["id"] == usuario_id) and
+            (nombre is None or usuario["nombre"].lower() == nombre.lower()) and
+            (edad is None or usuario["edad"] == edad)
+        ):
+            resultados.append(usuario)
+
+    if resultados:
+        return {"usuarios_encontrados": resultados}
+    else:
+        return {"mensaje": "No se encontraron usuarios que coincidan con los parámetros proporcionados."}
