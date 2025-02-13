fd3dc29e5ee016b9f39bb9b42f88b04e11a97c2d
Arriaga2 <163058678+Arriaga2@users.noreply.github.com>
2025-02-13 17:22:28 -0600

    tareas

--- TAREAS/main.py
+++ TAREAS/main.py
@@ -8,8 +8,20 @@ app = FastAPI(
     version='1.0.1'
 )
 
+
+
+tareas = [
+    {"id": 1, "titulo": "Comprar víveres", "descripcion": "Comprar leche y pan", "vencimiento": "15-02-24", "estado": "no completada"},
+    {"id": 2, "titulo": "Estudiar para el examen", "descripcion": "Repasar los apuntes de matemáticas", "vencimiento": "16-02-24", "estado": "no completada"},
+    {"id": 3, "titulo": "Hacer ejercicio", "descripcion": "Correr 5 km en el parque", "vencimiento": "17-02-24", "estado": "no completada"},
+    {"id": 4, "titulo": "Leer un libro", "descripcion": "Terminar el capítulo 4 de El Principito", "vencimiento": "18-02-24", "estado": "no completada"},
+    {"id": 5, "titulo": "Llamar a mamá", "descripcion": "Hablar con mamá por videollamada", "vencimiento": "19-02-24", "estado": "no completada"}
+]
+
+
 # Endpoint home
 @app.get('/', tags=['Calando el rollo'])
 def home():
     return {'hello': 'world FastAPI'}
+
 
