760db81390b6624253a939c8e2f06dd523bad74c
Arriaga2 <163058678+Arriaga2@users.noreply.github.com>
2025-02-17 18:02:51 -0600

    punto a y b

--- TAREAS/main.py
+++ TAREAS/main.py
@@ -24,4 +24,16 @@ tareas = [
 def home():
     return {'hello': 'world FastAPI'}
 
+# endpoint para obtenr todas las tareas
+@app.get('/tareas', tags=['Gestion Tareas'])
+def obtener_tareas():
+    return {"tareas": tareas }
 
+# endpoint para obtener una tarea especfica por ID
+@app.get('/tareas/{tarea_id}', tags=['Gestion Tareas'])
+def obtener_tarea(tarea_id: int):
+    for tarea in tareas:
+        if tarea['id'] == tarea_id:
+            return {"tarea": tarea}
+    raise HTTPException(status_code=404, detail="Esta taera no existe")
+    
